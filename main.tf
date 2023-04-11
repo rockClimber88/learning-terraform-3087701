8e7cfb48cf499feb10f04d0c2f8e992fb6d603b1
rockClimber88 <55462751+rockClimber88@users.noreply.github.com>
2023-04-11 16:17:09 -0400

    Add nat gateway

--- main.tf
+++ main.tf
@@ -14,6 +14,12 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+resource "aws_eip" "nat" {
+  count = 3
+
+  vpc = true
+}
+
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -24,6 +30,9 @@ module "blog_vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
+  single_nat_gateway  = false
+  reuse_nat_ips       = true                    # <= Skip creation of EIPs for the NAT Gateways
+  external_nat_ip_ids = "${aws_eip.nat.*.id}"   # <= IPs specified here as input to the module
 
   tags = {
     Terraform = "true"
